e38291d19986715e7f74105ced0bc7cad73aaaf4
Chris Brannen <cbrannen@outlook.com>
2022-04-18 21:04:19 +0100

    Initial Commit

---
+++ schemas/objects/index.js
@@ -0,0 +1,11 @@
+export { default as cta } from "./cta";
+export { default as embedHTML } from "./embedHTML";
+export { default as figure } from "./figure";
+export { default as internalLink } from "./internalLink";
+export { default as link } from "./link";
+export { default as portableText } from "./portableText";
+export { default as simplePortableText } from "./simplePortableText";
+export { default as hero } from "./hero";
+export { default as imageSection } from "./imageSection";
+export { default as mailchimp } from "./mailchimp";
+export { default as textSection } from "./textSection";
